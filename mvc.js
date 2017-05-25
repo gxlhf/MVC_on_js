49ccdab9cb0db4b5f23d31178144fd07872e4523
gxlhf <gx.lhf@163.com>
2017-05-26 00:17:51 +0800

    增加球的立体效果，改进参数输入

--- mvc.js
+++ mvc.js
@@ -10,11 +10,9 @@ Event.prototype = {
   constaructor: 'Event',
   attach : function(listeners) {
     this._listeners.push(listeners);
-    // console.log(this._listeners);
   },
   notify: function(objs){
     for(var i = 0,ilen = this._listeners.length; i < ilen; i+=1) {
-      // console.log(this._listeners[i]);
       this._listeners[i](this._observer,objs);
     }
   }
@@ -80,6 +78,7 @@ function View(_model, _elements) {
   this.changeSphere = new Event(this);
 
   var view = this;
+  this.isMouseDown = false;
 
   // 绑定模型监听器
   this.model.changeSphere.attach(function () {
@@ -87,30 +86,30 @@ function View(_model, _elements) {
   }),
 
   // 把监听器绑定到HTML事件上
-  this.elements.radius.change(function (e) {
-    view.changeSphere.notify({'valueName': 'radius', 'value' :e.target.value});
+  this.elements.radius.bind('input', function (e) {
+    view.changeSphere.notify({'valueName': 'radius', 'value': e.target.value});
   });
-  this.elements.volume.change(function (e) {
-    view.changeSphere.notify({'valueName': 'volume', 'value' :e.target.value});
+  this.elements.volume.bind('input', function (e) {
+    view.changeSphere.notify({'valueName': 'volume', 'value': e.target.value});
   });
-  this.elements.suArea.change(function (e) {
-    view.changeSphere.notify({'valueName': 'suArea', 'value' :e.target.value});
+  this.elements.suArea.bind('input', function (e) {
+    view.changeSphere.notify({'valueName': 'suArea', 'value': e.target.value});
   });
+
   this.elements.graphView.click(function (e) {
-    // 获取点击位置和调整后的半径数据
-    var x = e.clientX - view.elements.graphView.offset().left,
-        y = e.clientY - view.elements.graphView.offset().top;
-    var width = 600, height = 600;
-    var r = Math.sqrt(Math.pow(width / 2 - x, 2) + Math.pow(height / 2 - y, 2));
-    // console.log(typeof view.elements.graphView.offset().top);
-    // console.log("graphView", view.elements.graphView.offset());
-    // console.log("client", e.clientX, e.clientY);
-    // console.log("absolute", x, y, r
-    //             // e.clientX - view.elements.graphView.offset().left,
-    //             // e.clientY - view.elements.graphView.offset().top
-    //            );
-    // 
-    view.changeSphere.notify({'valueName': 'radius', 'value' :r});
+    view.changeSphere.notify({'valueName': 'radius', 'value': view.getR(e)});
+  });
+  this.elements.graphView.mousedown(function (e) {
+    view.isMouseDown = true;
+    view.changeSphere.notify({'valueName': 'radius', 'value': view.getR(e)});
+  });
+  this.elements.graphView.mouseup(function (e) {
+    view.isMouseDown = false;
+    view.changeSphere.notify({'valueName': 'radius', 'value': view.getR(e)});
+  });
+  this.elements.graphView.mousemove(function (e) {
+    if(view.isMouseDown)
+    view.changeSphere.notify({'valueName': 'radius', 'value': view.getR(e)});
   });
 }
 
@@ -125,6 +124,14 @@ View.prototype = {
     // 更改图形
     this.elements.graphView.find("#graph").css("height", this.model.getRadius() * 2 + "px");
     this.elements.graphView.find("#graph").css("width", this.model.getRadius() * 2 + "px");
+  },
+
+  getR: function (e) {
+    // 获取点击/拖动时鼠标当前位置和调整后的半径数据
+    var x = e.clientX - this.elements.graphView.offset().left,
+        y = e.clientY - this.elements.graphView.offset().top;
+    var width = 600, height = 600;
+    return Math.sqrt(Math.pow(width / 2 - x, 2) + Math.pow(height / 2 - y, 2));
   }
 }
 
@@ -147,6 +154,10 @@ Controller.prototype = {
   constructor: 'Controller',
 
   updateModel: function (argument) {
+    var reg = /^((0{0,1}|[1-9][0-9]*)|(0|[1-9][0-9]*)+(.[0-9]+))$/;
+    if(!reg.test(argument.value)){
+      return;
+    }
     switch(argument.valueName){
       case 'radius': this.model.setRadius(argument.value); break;
       case 'volume': this.model.setVolume(argument.value); break;
@@ -156,16 +167,16 @@ Controller.prototype = {
 }
 
 
-var model;
-var view;
-var controller;
+// var model;
+// var view;
+// var controller;
 $(function () {
-  model = new Model(0);
-  view = new View(model, {
+  var model = new Model(0);
+  var view = new View(model, {
     'radius': $('#radius'),
     'volume': $('#volume'),
     'suArea': $('#suArea'),
     'graphView': $('#graphView')
   });
-  controller = new Controller(model, view);
+  var controller = new Controller(model, view);
 })
